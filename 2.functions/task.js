2ccb87ab1681523879b5d6a67e32cab86f8f45b5
Maria Tsvetkova <maria-french@mail.ru>
2021-09-12 18:06:57 +0300

    First commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,65 @@
+'use strict'
+
 // Задание 1
 function getArrayParams(arr) {
   let min,max,sum,avg;
 
-  // Ваш код
+  sum = 0;
+  min = Infinity;
+  max = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  avg = +(sum / arr.length).toFixed(2);
 
   return { min:min, max:max, avg:avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
+  let max = -Infinity;
 
-  // Ваш кода
-  // for ...
-  
+  for (let i = 0; i < arrOfArr.length; i++) {
+    func(arrOfArr[i]);
+    if (func(arrOfArr[i]) > max) {
+      max = func(arrOfArr[i]);
+    }
+  }
+
   return max
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let max = -Infinity;
+  let min = Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+
+  let dif = +(max-min);
+
+  return dif
 }
