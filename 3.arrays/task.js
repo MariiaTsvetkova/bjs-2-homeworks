d800d34af9ee2f04a0e5504843342a967b85dacd
Maria Tsvetkova <maria-french@mail.ru>
2021-09-13 18:35:51 +0300

    First commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,22 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) return false;
+
+  if (arr1.length !== arr2.length) return false;
+  
+  result = arr1.every((item, idx) => { 
+    if (arr2[idx] !== item) return false;
+    return true
+  }) 
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  
+  let resultArr = arr.filter((number) => number > 0 && number % 3 === 0).map((number) => number * 10);
 
-  // Ваш код
-
+  
   return resultArr; // array
 }
