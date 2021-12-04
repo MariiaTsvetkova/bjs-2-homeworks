5534fce61d3a24bac16ed651a32ccbdb887fe08a
Maria Tsvetkova <maria-french@mail.ru>
2021-12-04 22:27:30 +0300

    Initial commit

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,81 +1,73 @@
+// Задача 1
+
 class PrintEditionItem {
-    #state
+    #state;
 
     constructor(name, releaseDate, pagesCount) {
         this.name = name;
         this.releaseDate = releaseDate;
         this.pagesCount = pagesCount;
+
         this.state = 100;
         this.type = null;
     }
-
     fix() {
         this.state = this.#state *= 1.5;
     }
 
     set state(value) {
-        if (!this.#isNumber(value)) {
-            throw new Error("State should be a number");
-        }
-
         if (value < 0) {
             this.#state = 0;
-            return;
-        }
-
-        if (value > 100) {
+        } else if (value > 100) {
             this.#state = 100;
-            return;
-        }
-
-        this.#state = value;
+        } else this.#state = value;
     }
 
     get state() {
         return this.#state;
     }
-
-    #isNumber(value) {
-        return typeof(value) === 'number' && !isNaN(value);
-    }
 }
 
-class Magazine extends PrintEditionItem{
+const sherlock = new PrintEditionItem("Sherlock Holmes", 2019, 1000);
+
+class Magazine extends PrintEditionItem {
     constructor(name, releaseDate, pagesCount) {
         super(name, releaseDate, pagesCount);
         this.type = "magazine";
     }
 }
 
-class Book extends PrintEditionItem{
+class Book extends PrintEditionItem {
     constructor(author, name, releaseDate, pagesCount) {
         super(name, releaseDate, pagesCount);
         this.author = author;
         this.type = "book";
     }
+    
 }
 
-class NovelBook  extends Book{
+class NovelBook extends Book {
     constructor(author, name, releaseDate, pagesCount) {
         super(author, name, releaseDate, pagesCount);
         this.type = "novel";
     }
 }
 
-class FantasticBook  extends Book{
+class FantasticBook extends Book {
     constructor(author, name, releaseDate, pagesCount) {
         super(author, name, releaseDate, pagesCount);
         this.type = "fantastic";
     }
 }
 
-class DetectiveBook  extends Book{
+class DetectiveBook extends Book {
     constructor(author, name, releaseDate, pagesCount) {
         super(author, name, releaseDate, pagesCount);
         this.type = "detective";
     }
 }
 
+// Задача 2
 
 class Library {
     constructor(name) {
@@ -84,177 +76,283 @@ class Library {
     }
 
     addBook(book) {
-        this.#validateBook(book);
-
         if (book.state > 30) {
             this.books.push(book);
         }
     }
 
     findBookBy(type, value) {
-        return this.books.find(book => this.#isBookAppropriate(book, type, value));
+        
 
-        for (let book of this.books)
-        {
-            if (this.#isBookAppropriate(book, type, value)) {
-                return book;
-            }
-        }
-        return null;
     }
 
     giveBookByName(bookName) {
-        const type = 'name'
-        const booksAmount = 1;
-        let book = this.findBookBy(type, bookName);
 
-        if (book === null) {
-            return null;
-        }
-
-        let index = this.books.indexOf(book);
-
-        return this.books.splice(index, booksAmount)[0];
-    }
-
-
-    #isBookAppropriate(book, type, value) {
-        return this.#readProperty(book, type) === value;
-    }
-
-    #readProperty(object, property) {
-        return object[property];
-    }
-
-    #validateBook(book) {
-        if (!book instanceof PrintEditionItem) {
-            throw new Error("Invalid argument: book should be instance of  PrintEditionItem");
-        }
     }
 }
 
-class Student {
-    #name;
-    #subjects;
+const library = new Library("Библиотека имени Ленина");
 
-    constructor(name) {
-        this.#name = name;
-        this.#subjects = new Array();
-    }
 
-    getName() {
-        return this.#name
-    }
 
-    addMark(grade, subject) {
-        let concreteSubject = this.#getSubjectOrDefault(subject);
 
-        try{
-            if (concreteSubject === null) {
-                return this.#addNewSubject(subject, grade);
-            }
 
-            concreteSubject.addGrade(grade);
-            return concreteSubject.getCountOfGrades();
-        }
-        catch (error) {
-            return `Вы пытались поставить оценку "${grade}" по предмету "${subject}". 
-            Допускаются только числа от 1 до 5.`;
-        }
-    }
 
-    getAverageBySubject(subject) {
-        let concreteSubject = this.#getSubjectOrDefault(subject);
+// class PrintEditionItem {
+//     #state
 
-        if (concreteSubject === null) {
-            return 0;
-        }
+//     constructor(name, releaseDate, pagesCount) {
+//         this.name = name;
+//         this.releaseDate = releaseDate;
+//         this.pagesCount = pagesCount;
+//         this.state = 100;
+//         this.type = null;
+//     }
 
-        return concreteSubject.getAverage();
-    }
+//     fix() {
+//         this.state = this.#state *= 1.5;
+//     }
 
-    getAverage() {
-        const length = this.#subjects.length;
-        if (length === 0) {
-            return 0;
-        }
+//     set state(value) {
+//         if (!this.#isNumber(value)) {
+//             throw new Error("State should be a number");
+//         }
 
-        let summaryAverages = 0;
-        this.#subjects.forEach(subject => {
-            summaryAverages += subject.getAverage();
-        });
+//         if (value < 0) {
+//             this.#state = 0;
+//             return;
+//         }
 
-        return summaryAverages / length;
-    }
+//         if (value > 100) {
+//             this.#state = 100;
+//             return;
+//         }
 
-    #getSubjectOrDefault(subjectName) {
-        for (let subject of this.#subjects) {
-            if (subject.name === subjectName) {
-                return subject;
-            }
-        }
+//         this.#state = value;
+//     }
 
-        return null;
-    }
+//     get state() {
+//         return this.#state;
+//     }
 
-    #addNewSubject(subjectName, grade) {
-        const subject = new Subject(subjectName);
-        this.#subjects.push(subject);
-        subject.addGrade(grade);
+//     #isNumber(value) {
+//         return typeof(value) === 'number' && !isNaN(value);
+//     }
+// }
 
-        return subject.getCountOfGrades();
-    }
-}
+// class Magazine extends PrintEditionItem{
+//     constructor(name, releaseDate, pagesCount) {
+//         super(name, releaseDate, pagesCount);
+//         this.type = "magazine";
+//     }
+// }
 
-class Subject {
-    #name;
-    #grades;
+// class Book extends PrintEditionItem{
+//     constructor(author, name, releaseDate, pagesCount) {
+//         super(name, releaseDate, pagesCount);
+//         this.author = author;
+//         this.type = "book";
+//     }
+// }
 
-    constructor(name) {
-        this.name = name;
-        this.#grades = new Array();
-    }
+// class NovelBook  extends Book{
+//     constructor(author, name, releaseDate, pagesCount) {
+//         super(author, name, releaseDate, pagesCount);
+//         this.type = "novel";
+//     }
+// }
 
-    get name() {
-        return this.#name;
-    }
+// class FantasticBook  extends Book{
+//     constructor(author, name, releaseDate, pagesCount) {
+//         super(author, name, releaseDate, pagesCount);
+//         this.type = "fantastic";
+//     }
+// }
 
-    set name(name) {
-        this.#name = name;
-    }
+// class DetectiveBook  extends Book{
+//     constructor(author, name, releaseDate, pagesCount) {
+//         super(author, name, releaseDate, pagesCount);
+//         this.type = "detective";
+//     }
+// }
 
-    addGrade(grade) {
-        if (this.#validateGrade(grade)) {
-            this.#grades.push(grade);
-        }
-    }
 
-    getCountOfGrades() {
-        return this.#grades.length;
-    }
+// class Library {
+//     constructor(name) {
+//         this.name = name;
+//         this.books = new Array();
+//     }
 
-    getAverage() {
-        const length = this.#grades.length;
+//     addBook(book) {
+//         this.#validateBook(book);
 
-        return length === 0 ?
-            0 : this.#getSumOfGrades() / length;
-    }
+//         if (book.state > 30) {
+//             this.books.push(book);
+//         }
+//     }
 
-    #getSumOfGrades() {
-        let result = 0;
-        this.#grades.forEach(grade => {
-            result += grade;
-        });
-        return result;
-    }
+//     findBookBy(type, value) {
+//         for (let book of this.books)
+//         {
+//             if (this.#isBookAppropriate(book, type, value)) {
+//                 return book;
+//             }
+//         }
+//         return null;
+//     }
 
-    #validateGrade(grade) {
-        const isValid = typeof(grade) === 'number' && grade >=1 && grade <= 5;
+//     giveBookByName(bookName) {
+//         const type = 'name'
+//         const booksAmount = 1;
+//         let book = this.findBookBy(type, bookName);
 
-        if (isValid) {
-            return true;
-        }
+//         if (book === null) {
+//             return null;
+//         }
 
-        throw new Error('Grade should be a number between 1 and 5');
-    }
-}
+//         let index = this.books.indexOf(book);
+
+//         return this.books.splice(index, booksAmount)[0];
+//     }
+
+
+//     #isBookAppropriate(book, type, value) {
+//         return this.#readProperty(book, type) === value;
+//     }
+
+//     #readProperty(object, property) {
+//         return object[property];
+//     }
+
+//     #validateBook(book) {
+//         if (!book instanceof PrintEditionItem) {
+//             throw new Error("Invalid argument: book should be instance of  PrintEditionItem");
+//         }
+//     }
+// }
+
+// class Student {
+//     #name;
+//     #subjects;
+
+//     constructor(name) {
+//         this.#name = name;
+//         this.#subjects = new Array();
+//     }
+
+//     getName() {
+//         return this.#name
+//     }
+
+//     addMark(grade, subject) {
+//         let concreteSubject = this.#getSubjectOrDefault(subject);
+
+//         try{
+//             if (concreteSubject === null) {
+//                 return this.#addNewSubject(subject, grade);
+//             }
+
+//             concreteSubject.addGrade(grade);
+//             return concreteSubject.getCountOfGrades();
+//         }
+//         catch (error) {
+//             return `Вы пытались поставить оценку "${grade}" по предмету "${subject}". 
+//             Допускаются только числа от 1 до 5.`;
+//         }
+//     }
+
+//     getAverageBySubject(subject) {
+//         let concreteSubject = this.#getSubjectOrDefault(subject);
+
+//         if (concreteSubject === null) {
+//             return 0;
+//         }
+
+//         return concreteSubject.getAverage();
+//     }
+
+//     getAverage() {
+//         const length = this.#subjects.length;
+//         if (length === 0) {
+//             return 0;
+//         }
+
+//         let summaryAverages = 0;
+//         this.#subjects.forEach(subject => {
+//             summaryAverages += subject.getAverage();
+//         });
+
+//         return summaryAverages / length;
+//     }
+
+//     #getSubjectOrDefault(subjectName) {
+//         for (let subject of this.#subjects) {
+//             if (subject.name === subjectName) {
+//                 return subject;
+//             }
+//         }
+
+//         return null;
+//     }
+
+//     #addNewSubject(subjectName, grade) {
+//         const subject = new Subject(subjectName);
+//         this.#subjects.push(subject);
+//         subject.addGrade(grade);
+
+//         return subject.getCountOfGrades();
+//     }
+// }
+
+// class Subject {
+//     #name;
+//     #grades;
+
+//     constructor(name) {
+//         this.name = name;
+//         this.#grades = new Array();
+//     }
+
+//     get name() {
+//         return this.#name;
+//     }
+
+//     set name(name) {
+//         this.#name = name;
+//     }
+
+//     addGrade(grade) {
+//         if (this.#validateGrade(grade)) {
+//             this.#grades.push(grade);
+//         }
+//     }
+
+//     getCountOfGrades() {
+//         return this.#grades.length;
+//     }
+
+//     getAverage() {
+//         const length = this.#grades.length;
+
+//         return length === 0 ?
+//             0 : this.#getSumOfGrades() / length;
+//     }
+
+//     #getSumOfGrades() {
+//         let result = 0;
+//         this.#grades.forEach(grade => {
+//             result += grade;
+//         });
+//         return result;
+//     }
+
+//     #validateGrade(grade) {
+//         const isValid = typeof(grade) === 'number' && grade >=1 && grade <= 5;
+
+//         if (isValid) {
+//             return true;
+//         }
+
+//         throw new Error('Grade should be a number between 1 and 5');
+//     }
+// }
