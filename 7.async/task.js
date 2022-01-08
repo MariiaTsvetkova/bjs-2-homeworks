25d8e0174b572eff62daed732e42b58a04eb92be
Maria Tsvetkova <maria-french@mail.ru>
2022-01-08 19:53:09 +0300

    add testCase

--- 7.async/task.js
+++ 7.async/task.js
@@ -54,3 +54,25 @@ class AlarmClock {
         this.alarmCollection = [];
     }
 }
+
+function testCase() {
+    let phoneAlarm = new AlarmClock();
+    phoneAlarm.addClock("23:37", () => console.log(`Пора вставать`), 1);
+    phoneAlarm.addClock("23:38", () => {
+        console.log(`Давай, вставай уже!`);
+        phoneAlarm.removeClock(2)
+    }, 2);
+    //phoneAlarm.addClock("09:01", () => console.log(`Иди умываться!`));
+
+    phoneAlarm.addClock("23.37", () => {
+        console.log(`Вставай, а то проспишь!`);
+        phoneAlarm.clearAlarms();
+        phoneAlarm.printAlarms();
+    }, 3);
+
+    phoneAlarm.addClock("09:05", () => console.log("Вставай, а то проспишь!"), 1);
+
+    phoneAlarm.printAlarms();
+
+    phoneAlarm.start();
+}
